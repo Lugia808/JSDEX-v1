6b4a29d0560cbb3e378012bf58d17e2391f0cc00
Lugia808 <naelsilva808@gmail.com>
2023-07-22 01:47:32 -0300

    Atualizações
    Atualizei algumas coisas do design e acrescentei alguns elementos.
    Organizei o código do CSS também.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let n1, altura;
 
+
 async function homepokemons() {
 
 
@@ -25,7 +26,9 @@ async function homepokemons() {
             div1.className = 'card1'
 
             const title = document.createElement('h3')
-            title.innerText = data.name
+            
+
+            title.innerHTML = `${data.name}`;
             title.className = 'pkname'
 
             const body = document.createElement('p');
@@ -42,14 +45,17 @@ async function homepokemons() {
             img.className = 'pkimage'
             img.src = imagesrc
 
+            const hr = document.createElement('hr')
+            hr.className = 'hrcardmini'
 
-
-
-
-
-            div.appendChild(title)
+            const numberpk = document.createElement('p')
+            numberpk.className = 'numberpk'
+            numberpk.innerHTML = `(N° ${order})`
+            
+            div.appendChild(numberpk)
             div.appendChild(img)
-            div.appendChild(div1)
+            div.appendChild(title)
+            div.appendChild(hr)
             cardContainer.appendChild(div)
 
 
@@ -66,13 +72,32 @@ const loadingElement = document.querySelector("#loading");
 const postsContainer = document.querySelector("#posts-container");
 
 function digitar(x) {
-    n1 = x.toString().toLowerCase()
-    console.log(n1);
+    n0 = x.toString().toLowerCase()
+    console.log(n0);
+
+}
+function digitar2(x) {
+    n2 = x.toString().toLowerCase()
+    console.log(n2);
+
 }
 
+
+
 async function getPokemonData() {
-    const inputElement = document.getElementById("pokemon-name");
-    digitar(inputElement.value);
+    const inputElement1 = document.getElementById("searchBox1")
+    const inputElement2 = document.getElementById("searchBox2")
+
+
+    digitar(inputElement1.value)
+    digitar2(inputElement2.value)
+
+    if (n2 == '') {
+        n1 = n0.toString()
+    }
+    else if (n0 == '') {
+        n1 = n2.toString()
+    }
 
     let url = `https://pokeapi.co/api/v2/pokemon/${n1}`;
 
@@ -83,17 +108,18 @@ async function getPokemonData() {
         const div = document.createElement('div');
         div.className = 'divnormal'
 
+
         const title = document.createElement('h2');
+        title.style.textTransform = 'capitalize'
 
         const body = document.createElement('p');
         body.className = 'textin'
 
-        const link = document.createElement('a');
-        link.style.width = '8rem'
-
         const divpai = document.createElement('div')
         divpai.className = 'divpai'
 
+
+        
         var peso = data.weight
         var pesoEmkg = peso / 10 // converte o peso para Kg
 
@@ -102,9 +128,13 @@ async function getPokemonData() {
         var name1 = data.name
         var name = name1.toString()
 
-        //Nome do pokemon
         
-        var home = document.getElementsByClassName('lugiaimg')
+
+
+
+        //Nome do pokemon
+
+        var home = document.getElementsByClassName('sitewallpaper')
 
         //declara a imagem do pokemon, trocando de acordo com o pokemon selecionado
         var id = data.id
@@ -113,41 +143,98 @@ async function getPokemonData() {
         image.style.backgroundColor = 'transparent'
         image.className = 'imagepokemon'
         image.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${order}.png`
+        
 
-        title.innerText = `${name} (n. ${order})`;
+        title.innerText = `${name} (n. ${order}) `;
+
+
+        const texttype = document.createElement('p')
+        texttype.className = 'HP'
+        const types = data.types
+        const pktype = types[0].type.name
+        texttype.innerHTML = `Type: ${pktype}`;
+
+
+        const life = data.stats[0].base_stat
+        console.log(life)
+        const texthp = document.createElement('p')
+        texthp.className = 'texthp'
+
+        texthp.innerHTML = 'HP:'
+        //texthp.innerHTML = `HP:`;
+
+
+        const barfundo = document.createElement('div')
+        barfundo.className = 'barfundo'
+
+        const barfrente = document.createElement('div')
+        barfrente.className = 'barfrente'
+        
+        ability_bar(life)
+
+        function ability_bar(stats){
+
+            if(stats > 100){
+                stats = 100
+            }
+            barfundo.innerHTML = `<p id='hpporcentagem'>${stats}</p>`
+            barfrente.style.width = `${stats}%`;
+            console.log(stats)
+        }
+
+
+        const divdescription = document.createElement('div')
+        divdescription.className = 'divdescription'
+        
         //informações do pokemon
         //body.innerHTML = `Altura: ${height} m, Peso: ${pesoEmkg} Kg`;
 
+        //var searchdiv = document.getElementById('searchdiv')
 
-        //cria uma div
         const imageDiv = document.createElement('div');
-        imageDiv.classList.add('pokemon_image');
-        link.href = data.species.url;
-        link.innerText = "More info";
+        imageDiv.classList.add('pokemon_imagediv');
 
 
-        divpai.scrollIntoView({behavior: 'smooth'})
+        const hr = document.createElement('hr')
+        hr.className = 'hrcard'
 
         //estudar
         imageDiv.appendChild(image)
-        div.appendChild(title);
+        div.appendChild(title)
         div.appendChild(imageDiv)
-
+        div.appendChild(hr)
+        div.appendChild(divdescription)
+        divdescription.appendChild(texttype)
+        divdescription.appendChild(texthp)
+        divdescription.appendChild(barfundo)
+        barfundo.appendChild(barfrente)
         div.appendChild(body);
+
 
         divpai.appendChild(div)
         postsContainer.innerHTML = "";
         postsContainer.appendChild(divpai);
-        
+
+
+        setTimeout(() => {
+
+            const targetScrollPosition = divpai.getBoundingClientRect().top + window.scrollY + (-50)
+            window.scrollTo({ top: targetScrollPosition, behavior: 'smooth' });
+
+        }, 100);
+
     } catch (error) {
         console.error("Error fetching data:", error);
     }
 }
 
-//lê teclas
+//busca
+const inputElement99 = document.getElementById('searchBox1')
+const inputElement = document.getElementById("searchBox2");
+//botões
+const submitButton2 = document.getElementById("submitButton2");
+const submitButton1 = document.getElementById('submitButton1')
 
-const inputElement = document.getElementById("pokemon-name");
-const searchButton = document.getElementById("search-button");
 
 inputElement.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
@@ -155,8 +242,15 @@ inputElement.addEventListener("keyup", function (event) {
     }
 });
 
-searchButton.addEventListener("click", function () {
-    getPokemonData();
+inputElement99.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        getPokemonData();
+    }
 });
 
 
+submitButton1.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+        getPokemonData();
+    }
+});
